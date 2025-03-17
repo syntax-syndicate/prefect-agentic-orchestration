0b9bb57fc2deed92bf2340f1277aa0f638429be4
Devin Villarosa <devinvillarosa92.business@gmail.com>
2025-03-17 13:22:10 -0700

    wip

--- ui-v2/src/routes/blocks.tsx
+++ ui-v2/src/routes/blocks.tsx
@@ -1,19 +1,24 @@
-import { buildListFilterBlockDocumentsQuery } from "@/api/block-documents";
+import {
+	buildCountFilterBlockDocumentsQuery,
+	buildListFilterBlockDocumentsQuery,
+} from "@/api/block-documents";
 import { buildListFilterBlockTypesQuery } from "@/api/block-types";
+import { BlocksPage } from "@/components/blocks/blocks-page";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/blocks")({
 	component: RouteComponent,
-	loader: async ({ context: { queryClient } }) => {
+	loader: ({ context: { queryClient } }) => {
 		// ----- Critical data
 		return Promise.all([
 			queryClient.ensureQueryData(buildListFilterBlockTypesQuery()),
 			queryClient.ensureQueryData(buildListFilterBlockDocumentsQuery()),
+			queryClient.ensureQueryData(buildCountFilterBlockDocumentsQuery()),
 		]);
 	},
 	wrapInSuspense: true,
 });
 
 function RouteComponent() {
-	return "🚧🚧 Pardon our dust! 🚧🚧";
+	return <BlocksPage />;
 }
